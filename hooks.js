8580b06895c7cff468ee4024ec31d58aa8825b41
Paul Murff <25558342+murffious@users.noreply.github.com>
2022-03-15 15:45:15 -0600

    ADDINGHOOK

--- hooks.js
+++ hooks.js
@@ -49,3 +49,24 @@ export default FetchComponent;
 // need to research more - react tam should really have an official way to do this ...
 // https://reactjs.org/blog/2015/12/16/ismounted-antipattern.html 
 // https://medium.com/@shanplourde/avoid-react-state-update-warnings-on-unmounted-components-bcecf054e953 
+
+
+
+// hooks/useStateWithSessionStorage.ts
+
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+type ReturnType = [string, Dispatch<SetStateAction<string>>];
+
+export default function useStateWithSessionStorage(
+  storageKey: string // Needs to be unique
+): ReturnType {
+  const [value, setValue] = useState(sessionStorage.getItem(storageKey) ?? "");
+
+  useEffect(() => {
+    sessionStorage.setItem(storageKey, value);
+  }, [value, storageKey]);
+
+  return [value, setValue];
+}
+https://www.dolthub.com/blog/2021-12-15-client-side-storage-with-react/
